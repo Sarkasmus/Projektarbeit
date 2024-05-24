0fcbc310fce6b24d2635104b23670122476f6df3
mamun002 <marvin.mundt002@stud.fh-dortmund.de>
2024-05-24 21:09:31 +0200

    Not geändert

--- DemoProject/src/exec/OperatorNOT.java
+++ DemoProject/src/exec/OperatorNOT.java
@@ -6,7 +6,23 @@ public abstract class OperatorNOT extends Gatter{
         super(a, b);
     }
 
-    public boolean[] ausgabe(boolean a, boolean b) {
-        return new boolean[]{!a, !b};
+   // public boolean[] ausgabe(boolean a, boolean b) {
+    //    return new boolean[]{!a, !b};
+   // }
+    public boolean ausgabe(boolean a, boolean b){
+        if (a = true){
+            a= false;
+        }
+        if (a = false) {
+            a = true;
+
+        }
+        if (b = true){
+            b= false;
+        }
+        if (b = false) {
+            b = true;
+        }
+        return ausgabe (a, b);
     }
 }
