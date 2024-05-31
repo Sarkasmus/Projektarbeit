8d2784666d8bf5c3774c3cb429c2510b14835ec0
mamun002 <marvin.mundt002@stud.fh-dortmund.de>
2024-05-31 12:03:59 +0200

    getter und setter und not geändert. Super wird nicht mehr benötigt

--- DemoProject/src/exec/OperatorNOT.java
+++ DemoProject/src/exec/OperatorNOT.java
@@ -2,9 +2,6 @@ package exec;
 
 public abstract class OperatorNOT extends Gatter{
 
-    public OperatorNOT(boolean a, boolean b) {
-        super(a, b);
-    }
 
     public boolean[] ausgabe(boolean a, boolean b) {
         return new boolean[]{!a, !b};
